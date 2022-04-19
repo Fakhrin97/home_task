6e6986d9f7527ff420a521943de7f67f9a8d1252
Fakhrin97 <fakhrinya@code.edu.az>
2022-04-20 03:29:20 +0400

    methodsandclass

---
+++ CapitalCase/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static readonly object stringBuilder;
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter some words:");
+            string word = Console.ReadLine();
+            string LowerWord = word.ToLower();
+            string [] arr = LowerWord.Split();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string clone = arr[i];
+                string newclone=clone.Substring(0,1).ToUpper()+clone.Substring(1);
+                arr[i] = newclone;
+                
+            } 
+             string UpperCase =string.Join(" ", arr);
+             Console.WriteLine(UpperCase);   
+
+            
+            
+
+
+
+
+
+
+
+        }
+       
+    }
+}
---
+++ FristClass/FristClass/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace FristClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Fakihrin","Aliyev","P324",99,true );
+            Student student1 = new Student("Alim", "Qasimov", "P324", 70, true);
+            Student student2 = new Student("Uzeir", "Mehdizade", "P324", 89, false);
+            Student student3 = new Student("Bill", "Gates", "P324", 68, false);
+
+            Console.WriteLine(student.getInfo());
+            Console.WriteLine(student1.getInfo());
+            Console.WriteLine(student2.getInfo());
+            Console.WriteLine(student3.getInfo());
+
+        }
+    }
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public bool IsGraduent;
+
+        public Student(string name , string suranme , string group , byte point , bool isGraduent)
+        {
+            Name = name;
+            Surname = suranme;
+            Group = group;
+            Point= point;
+            IsGraduent = isGraduent;
+
+        }
+
+        public string getInfo()
+        {
+
+            if (IsGraduent==true)
+            {
+                Console.WriteLine("This student is graduet");
+                
+            }
+            else if (Point>80)
+            {
+                Console.WriteLine("This student is not graduet");
+                Console.WriteLine("You can take the second exam");
+            }
+            else
+            {
+                Console.WriteLine("This student is not graduet");
+                Console.WriteLine("You can not take the second exam");
+            }  
+            
+            
+            return $"Name: {Name} Surname: {Surname} Group: {Group} Point: {Point} \n";
+        }
+            
+            
+        
+    }
+}
